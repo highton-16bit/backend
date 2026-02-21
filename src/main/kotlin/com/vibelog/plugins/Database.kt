1bcf810ee94625df168e580ad29e83318a60ef48
Daybreak312 <ty82afg12@gmail.com>
2026-02-21 17:18:18 +0900

    feat: Migrate database from MySQL to Supabase PostgreSQL

--- src/main/kotlin/com/vibelog/plugins/Database.kt
+++ src/main/kotlin/com/vibelog/plugins/Database.kt
@@ -8,23 +8,30 @@ import com.zaxxer.hikari.HikariDataSource
 import com.vibelog.models.*
 
 fun Application.configureDatabase() {
-    // 해커톤용: MySQL 로컬 연결 (발표: Scalable RDBMS Cluster)
+    val config = environment.config.config("storage")
+    val driverClassName = config.property("driverClassName").getString()
+    val jdbcURL = config.property("jdbcURL").getString()
+    val user = config.property("user").getString()
+    val password = config.property("password").getString()
+
+    // Supabase (PostgreSQL) 전용 HikariCP 설정
     val hikariConfig = HikariConfig().apply {
-        this.driverClassName = "com.mysql.cj.jdbc.Driver"
-        this.jdbcUrl = "jdbc:mysql://localhost:3306/vibelog?serverTimezone=UTC&useSSL=false"
-        this.username = "root"
-        this.password = "" // 로컬 비밀번호 입력
-        maximumPoolSize = 10
+        this.driverClassName = driverClassName
+        this.jdbcUrl = jdbcURL
+        this.username = user
+        this.password = password
+        maximumPoolSize = 5 // 해커톤 환경 최적화
         isAutoCommit = false
-        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        transactionIsolation = "TRANSACTION_READ_COMMITTED" // PostgreSQL 표준
         validate()
     }
 
     val dataSource = HikariDataSource(hikariConfig)
     Database.connect(dataSource)
 
+    // 테이블 자동 생성 (PostgreSQL 스키마 준수)
     transaction {
-        SchemaUtils.create(Travels, TravelPlanItems, Posts, PostPhotoMappings)
+        SchemaUtils.create(Users, Travels, TravelPlanItems, TravelPhotos, Posts, PostPhotoMappings)
     }
 }
 
